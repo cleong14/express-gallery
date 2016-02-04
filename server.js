ec9fd9c9588a8f1410ba44acca65ed9789147bda
Chaz Leong <cleong14@gmail.com>
2016-02-03 21:55:33 -1000

    added login.jade template and got login page to work

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // 2. set up middleware
 // 3. set up routes
 
+// adding local authentication (4-)
+
 // external modules
 var bodyParser = require('body-parser');
 var express = require('express');
@@ -13,6 +15,12 @@ var passport = require('passport');
 // ==============1===============
 var BasicStrategy = require('passport-http').BasicStrategy; // want to use basic Authentication Strategy
 // ==============1===============
+// ==============4===============
+var LocalStrategy = require('passport-local').Strategy;
+// ==============4===============
+
+var session = require('express-session');
+var CONFIG = require('./config/config.json');
 
 // my modules
 var app = express();
@@ -27,7 +35,14 @@ app.set('views', 'views');
 // app.use(express.static('public'));
 
 // starts using middleware
+// ==============5===============
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(CONFIG.SESSION));
+
+app.use(passport.initialize());
+app.use(passport.session());
+// ==============5===============
+
 app.use(morgan('dev'));
 app.use(methodOveride('_method'));
 
@@ -35,14 +50,68 @@ app.use(methodOveride('_method'));
 // start using passport middleware
 var user = { username: 'chaz', password: 'password', email: 'chaz@mail.com' };
 
-passport.use(new BasicStrategy(
-  function (username, password, done) {
-    if ( !(username === user.username && password === user.password) ) {
-      return done(null, false);
-    }
-    return done(null, user);
-  }));
+// passport.use(new BasicStrategy(
+//   function (username, password, done) {
+//     if ( !(username === user.username && password === user.password) ) {
+//       return done(null, false);
+//     }
+//     return done(null, user);
+//   }));
 // ==============2===============
+
+// ==============6===============
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    var isAuthenticated = authenticate(username, password);
+
+    if(!isAuthenticated) { // not authenticated
+      return done(null. false);
+    }
+    return done(null, user); // authenticated
+  }));
+// ==============6===============
+
+// ==============7===============
+passport.serializeUser(function (user, done) {
+  // user passed in from local strategy
+  // user attached to req.user
+  return done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  return done(null, user);
+});
+// ==============7===============
+
+// ==============8===============
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+
+function authenticate (username, password) {
+  var CREDENTIALS = CONFIG.CREDENTIALS;
+  var USERNAME = CREDENTIALS.USERNAME;
+  var PASSWORD = CREDENTIALS.PASSWORD;
+
+  return (username === USERNAME &&
+          password === PASSWORD);
+}
+
+function isAuthenticated (req, res, next) {
+  // if not the return value of isAuthenticated();
+  if (!req.isAuthenticated()) {
+    return res.redirect('/login');
+  }
+  return next();
+}
+// ==============8===============
 
 // require in database
 var db = require('./models');
@@ -77,12 +146,22 @@ app.get('/gallery/new',
   });
 
 // ==============3===============
-app.get('/secret',
-  passport.authenticate('basic', { session: false }),
-  function (req, res) {
-    res.json(req.user);
-  });
+// app.get('/secret',
+//   passport.authenticate('basic', { session: false }),
+//   function (req, res) {
+//     res.json(req.user);
+//   });
 // ==============3===============
+
+// ==============9===============
+  app.get('/secret',
+    isAuthenticated,
+
+    function (req, res) {
+      res.render('secret');
+    }
+  );
+// ==============9===============
 
 // adding logout
 app.get('/logout', function (req, res) {
